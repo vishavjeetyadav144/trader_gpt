578a72c9deb92834eb81d44aa6ff2cf3406fe6d8
vishavjeet <vishavjeetyadav144@gmail.com>
2025-11-19 16:52:12 +0530

    master

--- crypto_trader/settings.py
+++ crypto_trader/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-b8r4ptxxv=hs%h9x(jd*31-#$!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = ["*"]#os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
